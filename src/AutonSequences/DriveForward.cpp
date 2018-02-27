afffa6f27ab566883e975477dc159bb280a7e4fb
Grace0 <gracekwak25@gmail.com>
2018-02-26 21:46:23 -0800

    Started auton

--- src/AutonSequences/DriveForward.cpp
+++ src/AutonSequences/DriveForward.cpp
@@ -7,7 +7,7 @@
 
 #include <AutonSequences/DriveForward.h>
 
-std::vector<std::vector<double> > full_refs = { {0.0} };
+std::vector<std::vector<double> > full_refs = { {} };
 
 void DriveForward::Generate() {
 
@@ -39,29 +39,55 @@ void DriveForward::Generate() {
 	pathfinder_modify_tank(trajectory, length, leftTrajectory, rightTrajectory,
 			wheelbase_width);
 
-	std::cout << "PATHFINDER MADE IT" << std::endl;
+	std::cout << "PATHFINDER MADE IT" << length << std::endl;
+
+//	for (int r = 0; r < length; r++) {
+//		for (int c = 0; c < 6; c++) {
+//			full_refs.at(r).push_back(0.0);
+//			std::cout << "filling" << std::endl;
+//		}
+//	}
+//
+//	std::cout << "Filled" << std::endl;
+//	std::cout << "rows " << sizeof(full_refs) << " cols "
+//			<< full_refs.at(0).size() << std::endl;
+
+	for(int i = 0; i < 1500; i++) {
+		full_refs.push_back({});
+	}
 
 	int l;
-	for (l = 0; l < length; l++) { ////yaw pos, left pos, right pos, yaw vel, left vel, right vel
+	for (l = 0; l < 1500; l++) { ////yaw pos, left pos, right pos, yaw vel, left vel, right vel //TODO: LENGTH
 		Segment sl = leftTrajectory[l];
-		full_refs.at(l).at(0) = sl.heading; //ZERO yaw vel gains
-		full_refs.at(l).at(1) = sl.position;
-		full_refs.at(l).at(3) = 0.0;
-		full_refs.at(l).at(4) = sl.velocity;
+		Segment sr = rightTrajectory[l];
+
+		full_refs.at(l).push_back((double)sl.heading); //ZERO yaw vel gains
+		full_refs.at(l).push_back((double)sl.position);
+		full_refs.at(l).push_back((double)sr.position);
+		full_refs.at(l).push_back(0.0);
+		full_refs.at(l).push_back((double)sl.velocity);
+		full_refs.at(l).push_back((double)sr.velocity);
+
+//		full_refs.at(l).at(0) = sl.heading; //ZERO yaw vel gains
+//		full_refs.at(l).at(1) = sl.position;
+//		full_refs.at(l).at(2) = sr.position;
+//		full_refs.at(l).at(3) = 0.0;
+//		full_refs.at(l).at(4) = sl.velocity;
+//		full_refs.at(l).at(5) = sr.velocity;
+
+	//	full_refs.push_back({});
+
+		std::cout << "l: " << l << std::endl;
 	}
 
-	int r;
-	for (r = 0; r < length; r++) { ////yaw pos, left pos, right pos, yaw vel, left vel, right vel
-		Segment sr = rightTrajectory[r];
-		full_refs.at(r).at(2) = sr.position; //ZERO yaw vel gains
-		full_refs.at(r).at(5) = sr.velocity;
-	}
+	std::cout << "filled full refs" << "  " << sizeof(full_refs) << " " << full_refs.at(0).size() << std::endl;
 
 	SmartDashboard::PutNumber("pathfinder points", sizeof(full_refs));
-	SmartDashboard::PutNumber("pathfinder point length", full_refs.at(0).size());
+	SmartDashboard::PutNumber("pathfinder point length",
+			full_refs.at(0).size());
 
 	for (int r = 0; r < sizeof(full_refs); r++) {
-		for (int c = 0; c < full_refs.at(0).size(); c++) {
+		for (int c = 0; c < full_refs.at(0).size(); c++) { //not printing the 6th
 			std::cout << full_refs.at(r).at(c) << " ";
 		}
 		std::cout << "" << std::endl;
