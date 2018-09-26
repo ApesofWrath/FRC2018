42860c35b6653cb6bffd9a3fefb8bd049b1f1723
Grace0 <gracekwak25@gmail.com>
2018-09-25 20:54:25 -0700

    Started inheritance

---
+++ src/main/cpp/Carriage.h
@@ -0,0 +1,15 @@
+#include "Elevator.h"
+
+class Carriage : public Elevator {
+
+public:
+
+	const double DOWN_POS = 0.01;
+	const double MID_POS = 0.01;
+	const double HPS_POS = 0.01;
+	const double UP_POS = 0.01;
+
+private:
+
+
+};
---
+++ src/main/cpp/MiddleStage.h
@@ -0,0 +1,15 @@
+#include "Elevator.h"
+
+class MiddleStage : public Elevator {
+
+public:
+
+	const double DOWN_POS = 0.005;
+	const double MID_POS = 0.668;
+	const double HPS_POS = 0.5;
+	const double UP_POS = 0.89;
+
+
+private:
+
+};
