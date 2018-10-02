dbd1981e8f9bb1c6de5e9fecf7dcc35ff5521fc9
Grace0 <gracekwak25@gmail.com>
2018-10-01 19:34:01 -0700

    General climber code

--- src/main/cpp/Climber.h
+++ src/main/cpp/Climber.h
@@ -10,10 +10,27 @@
 class Climber {
 public:
 
-	TalonSRX *canTalonClimber;
+	TalonSRX *talonClimber1;
+	TalonSRX *talonClimber2;
+
+	const int INIT_STATE_C_H = 0;
+	const int DOWN_STATE_C_H = 1;
+	const int UP_STATE_C_H = 2;
+	int climber_state = INIT_STATE_C_H;
 
 	Climber();
 
+void ClimberStateMachine();
+void Move();
+
+double GetClimberPosition();
+
+void StartClimberThread();
+static void ClimberWrapper(Climber *climber_);
+void EndClimberThread();
+
 };
 
-#endif /* BARREL_H_ */
+#endif /* CLIMBER_H_ */
+
+//compt bot is comp bt for everything
